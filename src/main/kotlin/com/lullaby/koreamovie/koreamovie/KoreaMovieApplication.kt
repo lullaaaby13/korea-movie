9fa399add19ab8b8ea5623e9b48d0ea8c6d34bf6
lullaaaby13 <sds901234@naver.com>
2023-12-06 23:32:16 +0900

    first commit

---
+++ src/main/kotlin/com/lullaby/koreamovie/koreamovie/KoreaMovieApplication.kt
@@ -0,0 +1,11 @@
+package com.lullaby.koreamovie.koreamovie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoreaMovieApplication
+
+fun main(args: Array<String>) {
+    runApplication<KoreaMovieApplication>(*args)
+}
---
+++ src/test/kotlin/com/lullaby/koreamovie/koreamovie/KoreaMovieApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lullaby.koreamovie.koreamovie
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KoreaMovieApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
